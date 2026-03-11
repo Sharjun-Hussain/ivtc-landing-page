056166794a9b1999e66c5c518d2b9938388f32bc
sharjun hussain <sharjoonibnuhussain99@gmail.com>
2026-03-11 16:21:09 +0530

    modify:commented the stats

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <BackgroundWrapper>
       <HeroWithMegaMenu />
-      <CampusStats />
+      {/* <CampusStats /> */}
       <UpcomingCourses />
       <CoursePathways />
       <LMSLogin />
